d3ea202114c32a87a3a79d46d6d193f970509d09
Nate <natehallpc@gmail.com>
2021-12-28 12:33:45 -0700

    primitive maximum radii implemented, preparing to fix ellipse bug

--- DrawCircle.cpp
+++ DrawCircle.cpp
@@ -2,54 +2,65 @@
 #include <iostream>
 #include <cmath>
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
+    double horizVertRatio = 2.0;
+
     // Check if the radius is valid
-    if (std::stoi(argv[1]) < 2){
-        std::cout << "The minimum radius is 2.\nFor more help, type \"./DrawCircle help\"";
+    if (std::stoi(argv[1]) < 2)
+    {
+        std::cout << "The minimum radius is 2.\nFor more help, type \"./DrawCircle help\"" << std::endl;
+        return EXIT_FAILURE;
+    } else if ((std::stoi(argv[1]) * horizVertRatio) > 80) {
+        std::cout << "The maximum radius is " << ((80 / horizVertRatio) - 1) << ".\nFor more help, type \"./DrawCircle help\"" << std::endl;
         return EXIT_FAILURE;
     }
 
     Circle newCircle(std::stoi(argv[1]));
 
     // Set the character to be printed as the edges of the circle.
-    // If the edge argument is invalid or unspecified, use '.' as the edge character. 
+    // If the edge argument is invalid or unspecified, use '.' as the edge character.
     char edgeChar = '.';
-    if (argc == 3){
+    if (argc == 3)
+    {
         std::string charArg = argv[2];
-        if (charArg.length() > 1){
+        if (charArg.length() > 1)
+        {
             std::cout << "Edge parameter (second argument) should be a single character.\nFor more help, type \"./DrawCircle help\"" << std::endl;
-        } else {
-            edgeChar = charArg[0];
+            return EXIT_FAILURE;
         }
+        edgeChar = charArg[0];
     }
 
     // Print the circle.
-    std::cout << newCircle.toString(edgeChar) << std::endl;
+    std::cout << newCircle.toString(edgeChar, horizVertRatio) << std::endl;
     return EXIT_SUCCESS;
 }
 
-Circle::Circle(int radius){
+Circle::Circle(int radius)
+{
+    
     this->radius = radius;
-    this->diameter = (radius * 2) + 1;
+    this->diameter = ((radius * 2) + 1);
 }
 
-
-std::string Circle::toString(char edgeChar){
+std::string Circle::toString(char edgeChar, double horizVertRatio)
+{
     std::string circleString = "";
-    for (int i = 0; i < diameter; i++){
-        circleString += getLine(i, edgeChar);
+    for (int i = 0; i < diameter; i++)
+    {
+        circleString += getLine(i, edgeChar, horizVertRatio);
         if (i < diameter - 1)
             circleString += '\n';
     }
     return circleString;
 }
 
-std::string Circle::getLine(double index, char edgeChar){
-    
-    double theta = asin((radius - index )/ radius);
+std::string Circle::getLine(double index, char edgeChar, double horizVertRatio)
+{
+    double theta = asin((radius - index) / radius);
     std::cout << "Theta: " << theta << std::endl;
     int x = (cos(theta) * radius);
-  
     std::cout << "x: " << x << std::endl;
     int precedingSpaces = radius - x;
     std::cout << "Printing this edge at index " << precedingSpaces << std::endl;
@@ -57,8 +68,9 @@ std::string Circle::getLine(double index, char edgeChar){
     for (int i = 0; i < precedingSpaces; i++)
         line += ' ';
     line += edgeChar;
-    if (precedingSpaces != radius){
-        for (int i = 0; i < ((radius - precedingSpaces) * 2) + 1; i++)
+    if (precedingSpaces != radius)
+    {
+        for (int i = 0; i < ((radius - precedingSpaces) * 2) - 1; i++)
             line += ' ';
         line += edgeChar;
     }
--- DrawCircle.h
+++ DrawCircle.h
@@ -4,9 +4,9 @@ class Circle
 {
 public:
     Circle(int radius);
-    std::string toString(char edgeChar);
+    std::string toString(char edgeChar, double horizVertRatio);
 private:
     double radius;
     double diameter;
-    std::string getLine(double index, char edgeChar);
+    std::string getLine(double index, char edgeChar, double horizVertRatio);
 };
